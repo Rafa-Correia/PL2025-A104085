1a78c6a7363925b47948c4eebed824e5ba642755
Rafa-Correia <rafaelazevedocorreia04@gmail.com>
2025-06-01 22:35:15 +0100

    Oh

---
+++ TPC2/main.py
@@ -0,0 +1,14 @@
+import sys
+
+#7 campos
+#nome, desc, anocriacao, periodo, compositor, duracao, id
+#desc pode ter varias linhas (ou seja, varios newlines)
+#depois do 7 campo, existe sempre um newline
+
+
+def main():
+
+    return
+
+if __name__ == "__main__":
+    main()
